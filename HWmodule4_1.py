f72de9babfc2b3d79593485f486049f4e560cdd5
DaNilShet <danil.vip010@gmail.com>
2024-09-06 02:56:03 +0300

    Add files via upload

---
+++ HWmodule4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+a1 = fake_divide(83, 4)
+a2 = fake_divide(0, 3)
+a3 = fake_divide(37, 9)
+a4 = fake_divide(16, 0)
+
+print(a1)
+print(a2)
+print(a3)
+print(a4)

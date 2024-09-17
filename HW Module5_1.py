5df01128f15772b79b5cb8f64900013f1ff86742
DaNilShet <danil.vip010@gmail.com>
2024-09-17 11:08:15 +0300

    Add files via upload

---
+++ HW Module5_1.py
@@ -0,0 +1,27 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        self.number_of_floors_max = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors_max or new_floor < (self.number_of_floors_max-(self.number_of_floors_max-1)):
+            print('Такого этажа не существует')
+        else:
+            self.number_of_floors = new_floor
+            print(f'вы в доме {self.name}, на этаже {self.number_of_floors}')
+
+house_1 = House('ЖК Поле чудес', 22)
+house_2 = House('ЖК Панорама', 8)
+
+print(f'Это дом {house_1.name}, в нем {house_1.number_of_floors} этажей\n')
+
+house_1.go_to(5)
+house_1.go_to(10)
+house_2.go_to(17)
+
+
+print(f'Это дом {house_2.name}, в нем {house_2.number_of_floors} этажей\n')
+house_2.go_to(1)
+house_2.go_to(2)
+house_2.go_to(10)
